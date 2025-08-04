eea3e4f06c0eff749be7040860a6802ab4d7fbb6
Raza Javaid <razajavaid2004@gmail.com>
2025-08-04 22:35:57 +0500

    Update guessthenumber.h

--- Guess The Number/include/guessthenumber.h
+++ Guess The Number/include/guessthenumber.h
@@ -1,6 +1,8 @@
 class GuessTheNumber {
     public:
+    void setColor(int color);
     void welcomeScreen();
+    void updateLeaderBoard(int attempts, double duration);
     void selectDifficulty(int& low, int& high);
     void giveHint(int target);
     void playGame(int& low, int& high);
