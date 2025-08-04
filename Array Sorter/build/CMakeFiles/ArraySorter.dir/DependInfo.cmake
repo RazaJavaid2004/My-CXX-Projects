30c0e155a074865b801810fe055eed84194b3436
Raza Javaid <razajavaid2004@gmail.com>
2025-08-04 07:37:05 +0500

    Create Build Files

---
+++ Array Sorter/build/CMakeFiles/ArraySorter.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/Courses/My Codes/My-Projects/C++ Projects/Array Sorter/src/arraySorter.cpp" "CMakeFiles/ArraySorter.dir/src/arraySorter.cpp.obj" "gcc" "CMakeFiles/ArraySorter.dir/src/arraySorter.cpp.obj.d"
+  "D:/Courses/My Codes/My-Projects/C++ Projects/Array Sorter/src/main.cpp" "CMakeFiles/ArraySorter.dir/src/main.cpp.obj" "gcc" "CMakeFiles/ArraySorter.dir/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Array Sorter/build/CMakeFiles/ArraySorter.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "ArraySorter.exe"
+  "ArraySorter.exe.manifest"
+  "ArraySorter.pdb"
+  "CMakeFiles/ArraySorter.dir/src/arraySorter.cpp.obj"
+  "CMakeFiles/ArraySorter.dir/src/arraySorter.cpp.obj.d"
+  "CMakeFiles/ArraySorter.dir/src/main.cpp.obj"
+  "CMakeFiles/ArraySorter.dir/src/main.cpp.obj.d"
+  "libArraySorter.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ArraySorter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
